cd122b8bd519a01cbcb4b51c4a57f237077c1390
ctrenks <chris@trenkas.com>
2025-01-27 21:53:36 -0500

    updated login screen

--- auth.ts
+++ auth.ts
@@ -1,20 +1,46 @@
-import NextAuth from "next-auth";
+import NextAuth, { type NextAuthOptions } from "next-auth";
 import { PrismaAdapter } from "@auth/prisma-adapter";
 import { prisma } from "@/prisma";
 
 import GoogleProvider from "next-auth/providers/google";
+import EmailProvider from "next-auth/providers/email";
+import { Resend } from "resend";
 
-import Resend from "next-auth/providers/resend";
-export const { auth, handlers, signIn, signOut } = NextAuth({
+const resend = new Resend(process.env.RESEND_API_KEY);
+
+export const authOptions: NextAuthOptions = {
   adapter: PrismaAdapter(prisma),
   providers: [
-    Resend({
-      from: process.env.EMAIL_FROM,
-      apiKey: process.env.RESEND_API_KEY,
-    }),
     GoogleProvider({
       clientId: process.env.GOOGLE_CLIENT_ID!,
       clientSecret: process.env.GOOGLE_CLIENT_SECRET!,
     }),
+    EmailProvider({
+      server: {
+        host: process.env.EMAIL_SERVER_HOST,
+        port: Number(process.env.EMAIL_SERVER_PORT),
+        auth: {
+          user: process.env.EMAIL_SERVER_USER,
+          pass: process.env.EMAIL_SERVER_PASSWORD,
+        },
+      },
+      from: process.env.EMAIL_FROM,
+      sendVerificationRequest: async ({ identifier, url }) => {
+        try {
+          await resend.emails.send({
+            from: process.env.EMAIL_FROM!,
+            to: identifier,
+            subject: "Sign in to Your Account",
+            html: `<p>Click <a href="${url}">here</a> to sign in to your account.</p>`,
+          });
+        } catch (error) {
+          console.error("Error sending verification email:", error);
+          throw new Error("Failed to send verification email");
+        }
+      },
+    }),
   ],
-});
+  pages: {
+    signIn: "/auth/login",
+  },
+};
