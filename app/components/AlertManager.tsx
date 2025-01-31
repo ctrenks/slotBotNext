253541d506fb12584f1eafb9cd773acc991f7aec
ctrenks <chris@trenkas.com>
2025-01-30 21:53:23 -0500

    removed unused imp

--- app/components/AlertManager.tsx
+++ app/components/AlertManager.tsx
@@ -3,7 +3,7 @@
 import { useState, useEffect } from "react";
 import { useForm } from "react-hook-form";
 import { createAlert } from "@/app/actions/alert";
-import { getSlots, getSlotsForCasino } from "@/app/actions/slots";
+import { getSlotsForCasino } from "@/app/actions/slots";
 
 interface AlertFormData {
   message: string;
