707d4dc3ed7b284cb81e1213e5cffc659f839281
ctrenks <chris@trenkas.com>
2025-01-30 19:03:52 -0500

    update

--- app/api/casinos/[casinoId]/slots/route.ts
+++ app/api/casinos/[casinoId]/slots/route.ts
@@ -1,13 +1,13 @@
-import { NextResponse } from "next/server";
+import { NextRequest, NextResponse } from "next/server";
 import { getSlots } from "@/app/actions/slots";
 import { Slot, SlotError } from "@/app/types/slot";
 
 export async function GET(
-  _request: Request,
-  context: { params: { casinoId: string } }
-): Promise<NextResponse<Slot[] | SlotError>> {
+  req: NextRequest,
+  { params }: { params: { casinoId: string } }
+) {
   try {
-    const games = await getSlots(parseInt(context.params.casinoId));
+    const games = await getSlots(parseInt(params.casinoId));
     return NextResponse.json(games);
   } catch (error) {
     console.error("Failed to fetch slots:", error);
